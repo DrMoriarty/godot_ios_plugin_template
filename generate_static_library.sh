e73435ade5e3d6c973e81852e4553b5e0ea90425
Vasiliy Makarov <drmoriarty.0@gmail.com>
2021-03-03 14:36:15 +0300

    fix building process, add nativelib metafile

--- generate_static_library.sh
+++ generate_static_library.sh
@@ -6,19 +6,40 @@ SCHEME=${2:-godot_plugin}
 xcodebuild archive \
     -project "./$PROJECT" \
     -scheme $SCHEME \
-    -archivePath "./bin/ios.xcarchive" \
+    -archivePath "./bin/ios_release.xcarchive" \
     -sdk iphoneos \
     SKIP_INSTALL=NO
 
 xcodebuild archive \
     -project "./$PROJECT" \
     -scheme $SCHEME \
-    -archivePath "./bin/ios_sim.xcarchive" \
+    -archivePath "./bin/sim_release.xcarchive" \
     -sdk iphonesimulator \
     -arch x86_64 \
     SKIP_INSTALL=NO
 
+xcodebuild archive \
+    -project "./$PROJECT" \
+    -scheme $SCHEME \
+    -archivePath "./bin/ios_debug.xcarchive" \
+    -sdk iphoneos \
+    SKIP_INSTALL=NO \
+    GCC_PREPROCESSOR_DEFINITIONS="DEBUG_ENABLED=1"
+
+xcodebuild archive \
+    -project "./$PROJECT" \
+    -scheme $SCHEME \
+    -archivePath "./bin/sim_debug.xcarchive" \
+    -sdk iphonesimulator \
+    -arch x86_64 \
+    SKIP_INSTALL=NO \
+    GCC_PREPROCESSOR_DEFINITIONS="DEBUG_ENABLED=1"
+
 # Creating a fat libraries for device and simulator
-lipo -create "./bin/ios.xcarchive/Products/usr/local/lib/libgodot_plugin.a" \
-     "./bin/ios_sim.xcarchive/Products/usr/local/lib/libgodot_plugin.a" \
-     -output "./bin/godot_plugin.a"
+lipo -create "./bin/ios_release.xcarchive/Products/usr/local/lib/lib${SCHEME}.a" \
+     "./bin/sim_release.xcarchive/Products/usr/local/lib/lib${SCHEME}.a" \
+     -output "./bin/${SCHEME}.release.a"
+
+lipo -create "./bin/ios_debug.xcarchive/Products/usr/local/lib/lib${SCHEME}.a" \
+     "./bin/sim_debug.xcarchive/Products/usr/local/lib/lib${SCHEME}.a" \
+     -output "./bin/${SCHEME}.debug.a"
